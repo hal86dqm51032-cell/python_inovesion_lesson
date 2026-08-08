956caf3564cfe8da78c41cf3147c337395a9fffc
唐田陽琉 <hal86.dqm51032@gmail.com>
2026-08-08 11:46:59 +0900

    リセットのメッセージの改良

--- lesson9_2/pages/page2.py
+++ lesson9_2/pages/page2.py
@@ -2,9 +2,12 @@ import streamlit as st
 # ボタンが押されたらsessionstateをリセットするページ
 st.title("データリセット")
 st.write("下のボタンを押すと、入力したデータをすべてリセットします")
-if st.button("リセット") and (st.session_state.user_name != "" or st.session_state.grade != "" or st.session_state.hobby != []):
-    st.session_state.user_name=""
-    st.session_state.grade=""
-    st.session_state.hobby=[]
+if st.button("リセット"):
+    if st.session_state.user_name != "" or st.session_state.grade != "" or st.session_state.hobby != []:
+        st.session_state.user_name=""
+        st.session_state.grade=""
+        st.session_state.hobby=[]
+    else:
+        st.error("リセットするデータはありません")
 else:
-    st.error("リセットするデータはありません")
+    st.error("リセットボタンが押されていません")
