2781a8aea20e2f153e3e61e9d0a19a851c2af0f9
唐田陽琉 <hal86.dqm51032@gmail.com>
2026-08-08 11:27:06 +0900

    データ共有用アプリに学年と趣味を追加

--- lesson9_2/main.py
+++ lesson9_2/main.py
@@ -3,9 +3,27 @@ st.title("ユーザー情報入力")
 
 if "user_name" not in st.session_state:
     st.session_state.user_name = ""
+ 
+if "grade" not in st.session_state:
+    st.session_state.grade=""
+
+if "hobby" not in st.session_state:
+    st.session_state.hobby=""
 
 name=st.text_input("あなたの名前を入力してください")
 if st.button("名前を保存"):
     st.session_state.user_name = name
     st.success("名前を保存しました")
 st.write(f"現在保存されている名前:{st.session_state.user_name}")
+
+grade=st.selectbox("学年を選択して下さい",["小5","小６","中１","中２","中３"])
+if st.button("学年を保存"):
+    st.session_state.grade=grade
+    st.success("学年を保存しました")
+st.write(f"現在保存されている学年:{st.session_state.grade}")
+
+hobby=st.multiselect("趣味を選択してください",["読書","スポーツ","ゲーム","音楽","絵画","その他"])
+if st.button("趣味を保存"):
+    st.session_state.hobby=hobby
+    st.success("趣味を保存しました")
+st.write(f"現在保存されている学年:{st.session_state.hobby}")
--- lesson9_2/pages/page1.py
+++ lesson9_2/pages/page1.py
@@ -2,8 +2,22 @@ import streamlit as st
 st.title("ユーザー情報表示ページ")
 if "user_name" in st.session_state and st.session_state.user_name:
     st.success(f"こんにちは、{st.session_state.user_name}さん")
-    st.write("メインページで入力された名前が正しく表示されています。")
-    st.balloons()
 else:
     st.error("ユーザー名が設定されていません")
     st.write("メインページで名前を入力してください")
+
+if "grade" in st.session_state and st.session_state.grade:
+    st.success(f"あなたの学年は{st.session_state.grade}ですね!")
+else:
+    st.error("学年が設定されていません")
+    st.write("メインページで学年を選択してください")
+
+if "hobby" in st.session_state and st.session_state.hobby:
+    st.success(f"あなたは{st.session_state.hobby}が好きなんですね!")
+else:
+    st.error("趣味が1つも選択されていません")
+    st.write("メインページで趣味を１つ以上選択してください")
+
+if "user_name" in st.session_state and st.session_state.user_name and "grade" in st.session_state and st.session_state.grade and "hobby" in st.session_state and st.session_state.hobby:
+    st.balloons()
+    # もしすべてのsession_stateが入力/選択されたら風船を飛ばす
