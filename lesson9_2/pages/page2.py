c38be3b23a29e862cb55b6ca03ac2f0a67ad482c
唐田陽琉 <hal86.dqm51032@gmail.com>
2026-08-08 12:50:46 +0900

    リセットボタンのif文の修正

--- lesson9_2/pages/page2.py
+++ lesson9_2/pages/page2.py
@@ -11,30 +11,34 @@ if st.button("全リセット"):
         st.success("すべてのデータをリセットしました")
     else:
         st.error("リセットするデータはありません")
-if st.button("名前リセット") and st.session_state.user_name != "":
-    st.session_state.user_name = ""
-    st.success("名前のデータをリセットしました")
-else:
-    st.error("名前はリセットされているか、保存されていません")
+if st.button("名前リセット"):
+    if st.session_state.user_name != "":
+        st.session_state.user_name = ""
+        st.success("名前のデータをリセットしました")
+    else:
+        st.error("名前はリセットされているか、保存されていません")
 
-if st.button("学年リセット") and st.session_state.grade != "":
-    st.session_state.grade = ""
-    st.success("学年のデータをリセットしました")
-else:
-    st.error("学年はリセットされているか、保存されていません")
+if st.button("学年リセット"):
+    if st.session_state.grade != "":
+        st.session_state.grade = ""
+        st.success("学年のデータをリセットしました")
+    else:
+        st.error("学年はリセットされているか、保存されていません")
 
-if st.button("趣味リセット") and st.session_state.hobby != []:
-    st.session_state.hobby = []
-    st.success("趣味をのデータをリセットしました")
-else:
-    st.error("趣味はリセットされているか、保存されていません")
+if st.button("趣味リセット"):
+    if st.session_state.hobby != []:
+        st.session_state.hobby = []
+        st.success("趣味のデータをリセットしました")
+    else:
+        st.error("趣味はリセットされているか、保存されていません")
 
-if st.button("年齢をリセット") and st.session_state.age != "":
-    st.session_state.age = ""
-    st.success("年齢のデータをリセットしました")
-else:
-    st.error("年齢のデータはリセットされているか、保存されていません")
+if st.button("年齢をリセット"):
+    if st.session_state.age != "":
+        st.session_state.age = ""
+        st.success("年齢のデータをリセットしました")
+    else:
+        st.error("年齢のデータはリセットされているか、保存されていません")
 
-if st.session_state.user_name == "" and st.session_state.grade == "" and st.session_state.hobby == "" and st.session_state.age == "":
+if st.session_state.user_name == "" and st.session_state.grade == "" and st.session_state.hobby == [] and st.session_state.age == "":
     st.error("リセットするデータはありません")
     st.write("メインページでデータを入力,保存してください")
